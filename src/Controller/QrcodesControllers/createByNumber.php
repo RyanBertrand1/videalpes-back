1202f50c24fb96d3b551d44332fe27b80b8370e3
yohan <yohan.madeira@gmail.com>
2020-02-13 14:13:40 +0100

    Add controller for QRCode (create_by_number)

---
+++ src/Controller/QrcodesControllers/createByNumber.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller\QrcodesControllers;
+
+
+use App\Entity\Qrcode;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ObjectManager;
+class createByNumber
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $numbers = $request->get('number');
+        $myVal = intval($numbers);
+        for($i = 0; $i < $myVal;$i++){
+            $qrcode = new Qrcode();
+
+            $this->em->persist($qrcode);
+        }
+        $this->em->flush();
+    }
+}
